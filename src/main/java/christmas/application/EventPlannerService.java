cb3e4ec6d7e114e86a0a957b57257479e2a2f563
dltjdgh0428 <dltjdgh0428@naver.com>
2023-11-11 03:31:15 +0900

    feat / test : 혜택 및 배지 부여 [비지니스 로직 처리 기능]
    혜택 및 배지가 부여되는 기능과 그에 관한 단위테스트가 구현되었습니다.

--- src/main/java/christmas/application/EventPlannerService.java
+++ src/main/java/christmas/application/EventPlannerService.java
@@ -1,14 +1,24 @@
 package christmas.application;
 
 import christmas.domain.Date;
+import christmas.domain.EventBadge;
+import christmas.domain.Order;
 import christmas.dto.DateDTO;
 
 public class EventPlannerService {
 
+    public String determineEventBadge(int totalDiscount) {
+        return EventBadge.getBadgeByAmount(totalDiscount);
+    }
 
-
+    public int calculateFinalAmount(Order order, int day) {
+        int totalBeforeDiscount = EventBenefitCalculator.calculateTotalBeforeDiscount(order);
+        int totalDiscount = EventBenefitCalculator.calculateTotalDiscount(order, day);
+        return totalBeforeDiscount - totalDiscount;
+    }
 
     public DateDTO convertToDateDto(Date date) {
         return new DateDTO(date.getDay());
     }
+
 }
---
+++ src/test/java/christmas/domain/EventBadgeTest.java
@@ -0,0 +1,86 @@
+package christmas.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class EventBadgeTest {
+    @Test
+    @DisplayName("혜택 금액에 따른 배지 부여 - 산타 배지")
+    public void getBadgeByAmount_Santa() {
+        // given
+        int amount = 25000; // 산타 배지 기준 이상
+
+        // when
+        String badge = EventBadge.getBadgeByAmount(amount);
+
+        // then
+        assertEquals("산타", badge);
+    }
+
+    @Test
+    @DisplayName("혜택 금액에 따른 배지 부여 - 별 배지")
+    public void getBadgeByAmount_Star() {
+        // given
+        int amount = 6000; // 별 배지 기준 이상, 트리 배지 기준 미만
+
+        // when
+        String badge = EventBadge.getBadgeByAmount(amount);
+
+        // then
+        assertEquals("별", badge);
+    }
+
+    @Test
+    @DisplayName("혜택 금액에 따른 배지 부여 - 트리 배지")
+    public void getBadgeByAmount_Tree() {
+        // given
+        int amount = 12000; // 트리 배지 기준 이상, 산타 배지 기준 미만
+
+        // when
+        String badge = EventBadge.getBadgeByAmount(amount);
+
+        // then
+        assertEquals("트리", badge);
+    }
+
+    @Test
+    @DisplayName("혜택 금액에 따른 배지 부여 - 배지 없음")
+    public void getBadgeByAmount_None() {
+        // given
+        int amount = 3000; // 모든 배지 기준 미만
+
+        // when
+        String badge = EventBadge.getBadgeByAmount(amount);
+
+        // then
+        assertEquals("없음", badge);
+    }
+
+    @Test
+    @DisplayName("혜택 금액 정확히 별 배지 기준일 때")
+    public void getBadgeByAmount_ExactStar() {
+        // given
+        int amount = 5000; // 별 배지 기준 정확히
+
+        // when
+        String badge = EventBadge.getBadgeByAmount(amount);
+
+        // then
+        assertEquals("별", badge);
+    }
+
+    @Test
+    @DisplayName("혜택 금액 정확히 트리 배지 기준일 때")
+    public void getBadgeByAmount_ExactTree() {
+        // given
+        int amount = 10000; // 트리 배지 기준 정확히
+
+        // when
+        String badge = EventBadge.getBadgeByAmount(amount);
+
+        // then
+        assertEquals("트리", badge);
+    }
+}
